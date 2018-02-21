906f68ee41a31f7ad7a2772bc194d8c53f41a436
Judy McGoldrick <jjeanius@verizon.net>
2018-02-21 05:51:47 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -9,7 +9,7 @@ attr_accessor :player
 
   def move(board)
     binding.pry
-    if valid_move(board) == 5
+    if !board.taken?("5")
       "5"
     else
       best_move(board) + 1
@@ -17,15 +17,19 @@ attr_accessor :player
   end
 
   def best_move
-    valid_move
+    win(board) || block(board) || corner(board) || randon
   end
 
-  def valid_move
-    valid_move << computer_move
-    moves.each do|input|
-      input.move == valid_move
-    end
+  def corner(board)
+    [0,2,6,8].detect{|cell|} !board.taken?(cell+1)}
   end
 
-
+  def winning_combo?(board, token)
+      Game::WIN_COMBINATIONS.detect do |combo|
+        ((board.cells[combo[0]] == token && board.cells[combo[1]] == token) && !board.taken?(combo[2]+1)) ||
+        ((board.cells[combo[1]] == token && board.cells[combo[2]] == token) && !board.taken?(combo[0]+1)) ||
+        ((board.cells[combo[0]] == token && board.cells[combo[2]] == token) && !board.taken?(combo[1]+1))
+      end
+   end
+  
 end
